31b415f94d577518420c86b83e1ec96c032adeb7
nanbowan123 <33351427+nanbowan123@users.noreply.github.com>
2026-03-12 17:39:45 +0800

    Update updater.rs
    del r

--- src/updater.rs
+++ src/updater.rs
@@ -118,7 +118,6 @@ fn start_auto_update_check_(rx_msg: Receiver<UpdateMsg>) {
 }
 
 fn check_update(manually: bool) -> ResultType<()> {
-    return Ok(());
     #[cfg(target_os = "windows")]
     let is_msi = crate::platform::is_msi_installed()?;
     if !(manually || config::Config::get_bool_option(config::keys::OPTION_ALLOW_AUTO_UPDATE)) {
